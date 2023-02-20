5911c5de5b8122a58075b52d3e047ffca5ae8cf4
JHartmann-ims <julian.hartmann@students.ksba.ch>
2023-02-20 17:18:14 +0100

    installation of tools and setup

---
+++ Gluecksrad_M151/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import shoes from './assets/shoes.jpg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
