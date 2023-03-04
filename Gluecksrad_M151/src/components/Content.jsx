820e7a95aa922092c0cfd2ae430cbf90bc148420
JHartmann-ims <julian.hartmann@students.ksba.ch>
2023-03-04 19:40:58 +0100

    commit for pc

--- Gluecksrad_M151/src/components/Content.jsx
+++ Gluecksrad_M151/src/components/Content.jsx
@@ -1,17 +1,38 @@
-import React, { useState} from "react";
+import { collection, query } from "firebase/firestore";
+import React, { useState, useEffect } from "react";
+import {db} from "./firebase";
 
 
 import { AiFillHeart } from "react-icons/ai"
 
 const Content = () => {
   const [randomNumber, setRandomNumber] = useState(null);
-  const [lastRandomNumber, setLastRandomNumber] = useState(null);
+  const [lastRandomNumber, setLastRandomNumber] = useState('...');
+
+  useEffect(() => {
+    const q = query(collection(db, "todos"))
+  }, [])
+
+  const word = "Hallo";
+  const fields = [];
+
+  for (let i = 0; i < word.length; i++) {
+    fields.push(
+      <div className="w-10 h-10 bg-neutral flex justify-center items-center rounded-md mx-1" key={i}>
+        <div className="w-4 h-4 border-b-2 border-primary flex justify-center items-center">
+          <h1>{word.charAt(i).toUpperCase()}</h1>
+        </div>
+      </div>
+
+    )
+  }
+
 
   function getRandomNumber() {
     const randomNumber = Math.floor(Math.random() * 11) * 10;
     return randomNumber;
   }
-  
+
   function handleSpinClick() {
     const possibleMoney = getRandomNumber();
     setRandomNumber(possibleMoney === 0 ? 'dead' : possibleMoney);
@@ -26,11 +47,14 @@ const Content = () => {
           <AiFillHeart />
           <AiFillHeart className="" />
         </div>
+        <div className="flex">
+          {fields}
+        </div>
         <h1>Hello</h1>
         <div className="stats shadow bg-neutral">
           <div className="stat">
             <div className="stat-title">Prize</div>
-            <div className="stat-value">Money: {randomNumber}</div>
+            <div className="stat-value">{randomNumber}</div>
             <div className="stat-desc">last spin {lastRandomNumber}</div>
           </div>
         </div>
---
+++ Gluecksrad_M151/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBXTgRTEaMg5_ukrNM-CDrHPUqBYh1nWXs",
+  authDomain: "gluecksrad-m151.firebaseapp.com",
+  projectId: "gluecksrad-m151",
+  storageBucket: "gluecksrad-m151.appspot.com",
+  messagingSenderId: "386874730242",
+  appId: "1:386874730242:web:43f8926541685450e3041b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
